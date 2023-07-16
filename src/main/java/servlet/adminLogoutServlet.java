4316b18c33b2aa50abf7a946146f49d74379a2cc
Hazrul <2021816444@student.uitm.edu.my>
2023-07-16 11:17:52 +0800

    Add servlet for user logout and admin logout

---
+++ src/main/java/servlet/adminLogoutServlet.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Constant.userRole;
+import Utility.TrainUtil;
+
+/**
+ * Servlet implementation class adminLogoutServlet
+ */
+@WebServlet("/adminLogoutServlet")
+public class adminLogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+			if(TrainUtil.isLoggedIn(request, userRole.ADMIN)) {
+				
+				//call logout
+				TrainUtil.logout(response);
+				
+				RequestDispatcher rd=request.getRequestDispatcher("adminLogin.html");
+				rd.include(request, response);
+			}
+			else {
+				
+				RequestDispatcher rd=request.getRequestDispatcher("adminLogin.html");
+				rd.include(request, response);
+				
+			}
+		
+		
+		
+	}
+
+	
+	
+
+}
---
+++ src/main/java/servlet/userLogoutServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Constant.userRole;
+import Utility.TrainUtil;
+
+/**
+ * Servlet implementation class userLogoutServlet
+ */
+@WebServlet("/userLogoutServlet")
+public class userLogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		response.setContentType("text/html");
+		if(TrainUtil.isLoggedIn(request, userRole.CUSTOMER)) {
+			
+			//call logout
+			TrainUtil.logout(response);
+			
+			RequestDispatcher rd=request.getRequestDispatcher("index.html");
+			rd.include(request, response);
+		}
+		else {
+			//else also redirect to index page
+			RequestDispatcher rd=request.getRequestDispatcher("index.html");
+			rd.include(request, response);
+			
+		}
+	}
+
+	
+
+}
